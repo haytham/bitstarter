be5cab64105ae81980964bf5ece56136cf2b25e2
haytham <haytham.samad@gmail.com>
2013-07-14 00:46:26 +0000

    Loading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  fs.readFileSync('./index.html', function(err, buf) {
+	if(err) response.send(err);
+	response.send(buf.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
