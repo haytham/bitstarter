cbd656212efc9630fd81f7f449e3c8fa20e957eb
Ubuntu <ubuntu@ip-172-31-23-137.us-west-2.compute.internal>
2013-07-30 05:41:13 +0000

    Checking in partial work on mobile friendly version of site

--- web.js
+++ web.js
@@ -8,7 +8,12 @@ app.get('/', function(request, response) {
   response.send( fs.readFileSync( './index.html' ).toString() );
 });
 
-var port = process.env.PORT || 5000;
+// heroku
+//var port = process.env.PORT || 5000;
+
+// amazon ec2 port
+var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
